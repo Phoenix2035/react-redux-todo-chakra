dac23f26e8f11a8dd9efc991b881a369ef1f0e35
Ali Gh <aligodosi@gmail.com>
2022-01-21 22:06:03 +0330

    add redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,10 @@
-import { createStore } from "redux"
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+
 import rootReducer from "./rootReducer"
+
 
 export const store = createStore(
     rootReducer,
-)
-
+    composeWithDevTools()
+);
